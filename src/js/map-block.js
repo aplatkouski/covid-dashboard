e514e2e6be27bbcb5ffeb6143de165556ba2481c
aplatkouski <5857672+aplatkouski@users.noreply.github.com>
2020-12-22 05:55:03 +0300

    refactor: remove redundant mapbox-gl-js package
    Signed-off-by: aplatkouski <5857672+aplatkouski@users.noreply.github.com>

--- src/js/map-block.js
+++ src/js/map-block.js
@@ -72,6 +72,7 @@ export default class MapBlock {
     this.map = L.map('covid-map').setView(
       [defaultCountry.latitude, defaultCountry.longitude], 6,
     );
+
     L.tileLayer(
       'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
       {
