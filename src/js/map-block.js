7a867c7c8e2b3bced7b9193ac80ce3af2d90f8ae
aplatkouski <5857672+aplatkouski@users.noreply.github.com>
2020-12-22 03:58:04 +0300

    feat: add popup when clicking on county on map
    Signed-off-by: aplatkouski <5857672+aplatkouski@users.noreply.github.com>

--- src/js/map-block.js
+++ src/js/map-block.js
@@ -1,7 +1,5 @@
 import L from 'leaflet';
-import mapboxgl from 'mapbox-gl/dist/mapbox-gl';
-
-mapboxgl.accessToken = 'pk.eyJ1IjoiYXBsYXRrb3Vza2kiLCJhIjoiY2tpeHlyOWZwMThtYjJxbXd2cHRwajIyNyJ9.jbgJSxSYgjaS_moNI_RLgw';
+import countryFeatureCollection from './countries-feature-colletion';
 
 const settings = {
   mapbox: 'pk.eyJ1IjoiYXBsYXRrb3Vza2kiLCJhIjoiY2tpeHlyOWZwMThtYjJxbXd2cHRwajIyNyJ9.jbgJSxSYgjaS_moNI_RLgw',
@@ -10,6 +8,25 @@ const settings = {
   tileLayerAttribution: 'Map data &copy; <a'
     + ' href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     + ' contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  // eslint-disable-next-line no-unused-vars
+  featureStyle: {
+    color: 'white',
+    dashArray: '3',
+    fillColor: 'grey',
+    fillOpacity: 0,
+    opacity: 0,
+    weight: 1,
+  },
+  // eslint-disable-next-line no-unused-vars
+  selectedFeatureStyle: {
+    color: 'white',
+    dashArray: '3',
+    fillColor: 'grey',
+    stroke: true,
+    fillOpacity: 0.2,
+    opacity: 0.2,
+    weight: 2,
+  },
   lastDay: {
     confirmed: 'количество случаев заболевания за последний день в абсолютных величинах',
     deaths: 'количество летальных исходов за последний день в абсолютных величинах',
@@ -49,12 +66,14 @@ export default class MapBlock {
     this.selectCountryCallback = typeof selectCountryCallback === 'function'
       ? selectCountryCallback
       : () => {};
+    this.selectedCountry = undefined;
 
     this.map = L.map('covid-map').setView(
       [
         this.casesByCountry[this.settings.defaultCountryAlpha2Code].latitude,
-        this.casesByCountry[this.settings.defaultCountryAlpha2Code].longitude],
-      5,
+        this.casesByCountry[this.settings.defaultCountryAlpha2Code].longitude,
+      ],
+      6,
     );
     L.tileLayer(
       'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
@@ -68,31 +87,95 @@ export default class MapBlock {
         accessToken: this.settings.mapbox,
       },
     ).addTo(this.map);
-    Object.keys(this.casesByCountry).forEach((key) => {
-      this.addCircle(this.casesByCountry[key]);
+
+    this.getPopupContent = (country) => {
+      const covidStatisticsData = country[this.options.group][this.options.subGroup];
+      const popupMessage = this.settings[this.options.group][this.options.subGroup];
+
+      const $flagImage = document.createElement('img');
+      $flagImage.src = country.flagUrl;
+      $flagImage.alt = `${country.alpha2Code} flag`;
+      $flagImage.classList.add(this.settings.flagIconCSSClass);
+
+      const $h2 = document.createElement('h2');
+      $h2.appendChild($flagImage);
+      $h2.appendChild(document.createTextNode(country.name));
+      $h2.dataset.alpha2Code = country.alpha2Code;
+
+      const $p = document.createElement('p');
+      $p.appendChild(document.createTextNode(
+        `${covidStatisticsData} - ${popupMessage}`,
+      ));
+
+      const $container = document.createElement('div');
+      $container.appendChild($h2);
+      $container.appendChild($p);
+      return $container;
+    };
+
+    this.onEachFeature = (feature, layer) => {
+      const props = feature.properties;
+      if (
+        props.alpha2Code
+        && {}.hasOwnProperty.call(this.casesByCountry, props.alpha2Code)
+      ) {
+        const country = this.casesByCountry[props.alpha2Code];
+
+        const popup = L.popup();
+        popup.setContent(this.getPopupContent(country));
+
+        country.layer = layer;
+        country.layer.bindPopup(popup);
+        this.addCircle(country, popup);
+      }
+    };
+
+    this.geoJSONLayerGroup = L.geoJson(
+      countryFeatureCollection,
+      {
+        onEachFeature: this.onEachFeature,
+        style: this.settings.featureStyle,
+      },
+    ).addTo(this.map);
+
+    this.geoJSONLayerGroup.on('click', (e) => {
+      if (e.layer.feature.properties.alpha2Code) {
+        this.selectCountryCallback(e.layer.feature.properties.alpha2Code);
+      }
     });
   }
 
   selectCountry(alpha2Code) {
+    const country = this.casesByCountry[alpha2Code];
+
     this.map.flyTo(
-      [this.casesByCountry[alpha2Code].latitude, this.casesByCountry[alpha2Code].longitude],
+      [
+        this.casesByCountry[alpha2Code].latitude,
+        this.casesByCountry[alpha2Code].longitude,
+      ],
       5,
     );
+    // restore previous hidden circle
+    if (this.selectedCountry) {
+      const popup = L.popup();
+      popup.setContent(this.getPopupContent(this.selectedCountry));
+      this.addCircle(this.selectedCountry, popup);
+      this.geoJSONLayerGroup.resetStyle(this.selectedCountry.layer);
+    }
+
+    this.map.removeLayer(country.circle);
+    country.circle = undefined;
+
+    this.selectedCountry = country;
+    country.layer?.setStyle(this.settings.selectedFeatureStyle);
   }
 
-  addCircle(country) {
+  addCircle(country, popup) {
     const radius = country[this.options.group][this.options.subGroup] / 10;
-    const message = this.settings[this.options.group][this.options.subGroup];
-    const {
-      alpha2Code,
-      name,
-      latitude,
-      longitude,
-      flagUrl,
-    } = country;
 
-    const circle = L.circle(
-      [latitude, longitude],
+    const currentCountry = country;
+    currentCountry.circle = L.circle(
+      [country.latitude, country.longitude],
       {
         color: 'red',
         fillColor: '#f03',
@@ -100,20 +183,12 @@ export default class MapBlock {
         radius,
       },
     ).addTo(this.map);
-    const popup = L.popup();
-    popup.setContent(`<h2><img class="${this.settings.flagIconCSSClass}"
-    src="${flagUrl}" alt="${alpha2Code} flag">${name}</h2><br>
-    <p><b>${country[this.options.group][this.options.subGroup]}</b>
-    - ${message}</p>`);
-    circle.on('mouseover', (e) => {
+
+    currentCountry.circle.on('mouseover', (e) => {
       popup.setLatLng(e.latlng).openOn(this.map);
     });
-    circle.on('mouseout', () => {
+    currentCountry.circle.on('mouseout', () => {
       this.map.closePopup();
-    });
-
-    circle.on('click', () => {
-      this.selectCountryCallback(alpha2Code);
     });
   }
 
