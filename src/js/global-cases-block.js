ffa584eca62bbe03c9e5ed197aa3621959133222
aplatkouski <5857672+aplatkouski@users.noreply.github.com>
2020-12-22 23:46:20 +0300

    refactor: remove GlobalCasesBlock.selectCountry method
    Signed-off-by: aplatkouski <5857672+aplatkouski@users.noreply.github.com>

--- src/js/global-cases-block.js
+++ src/js/global-cases-block.js
@@ -16,13 +16,6 @@ export default class GlobalCasesBlock {
     this.$mainContainer.innerHTML = `<h2>${this.globalCasesData.total.confirmed}</h2>`;
   }
 
-  selectCountry(alpha2Code) {
-    this.$mainContainer.innerHTML = `<h2>
-    <img class="flag-icon" src="${this.casesByCountry[alpha2Code].flagUrl}" 
-    alt="${alpha2Code} flag">${
-  this.casesByCountry[alpha2Code][this.options.group][this.options.subGroup]}</h2>`;
-  }
-
   render() {
     this.$mainContainer.innerHTML = `<h2>${this.globalCasesData.total.confirmed}</h2>`;
   }
